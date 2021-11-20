b165d6c499feaea6d0343045bc6910c40b080689
ViyyapuSirisha <94158711+ViyyapuSirisha@users.noreply.github.com>
2021-11-20 21:47:50 +0530

    Update Calendar.c

--- 3_Implementation/Src/Calendar.c
+++ 3_Implementation/Src/Calendar.c
@@ -1,1 +1,11 @@
 
+int main(void)
+{
+	int year, daycode, leapyear;
+	
+	year = inputyear();
+	daycode = determinedaycode(year);
+	determineleapyear(year);
+	calendar(year, daycode);
+	printf("\n");
+}
