2cc2e43c1a23e1177bbcccdb208d27d6b362384b
ViyyapuSirisha <94158711+ViyyapuSirisha@users.noreply.github.com>
2021-11-21 23:33:39 +0530

    Update Calendar.c

--- 3_Implementation/Src/Calendar.c
+++ 3_Implementation/Src/Calendar.c
@@ -1,11 +1,24 @@
 
 int main(void)
 {
-	int year, daycode, leapyear;
-	
-	year = inputyear();
-	daycode = determinedaycode(year);
-	determineleapyear(year);
-	calendar(year, daycode);
-	printf("\n");
+//  int y3 = 2000;
+//  int y3 = 1946;
+//  printYear(y3);
+  int y;
+  int quit=0;
+
+  do
+    {
+        printf("Please enter which Year to Print For You (0 to quit): ");
+        scanf("%d",&y);
+        printf("\n");
+
+        if(y==0)
+            quit = 1;
+        else
+            printYear(y);
+
+    }while(!quit);
+
+  return 0;
 }
